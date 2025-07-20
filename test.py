42e1adb881de0a26aec8b5a94fde99cd5508e5d0
zoldyck13 <136193544+zoldyck13@users.noreply.github.com>
2025-07-20 10:54:40 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import json
 with open("Qwen_7B_Chat.ipynb", "r", encoding="utf-8") as f:
     nb = json.load(f)
 
-# حذف metadata.widgets إذا كانت موجودة
 if "widgets" in nb.get("metadata", {}):
     del nb["metadata"]["widgets"]
 
